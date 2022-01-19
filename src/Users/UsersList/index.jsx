3b8638e94eb67ea980f7bf16cde932673e4a5e54
Apoorvo Chakraborty <apoorvo.chak@gmail.com>
2022-01-19 23:39:34 +0530

    Fetched user data and added pagination.

---
+++ src/Users/UsersList/index.jsx
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from "react";
+import usersApi from "../../api/user";
+import Pagination from "./Pagination";
+import TableView from "./TableView";
+
+const UsersList = () => {
+  const [users, setUsers] = useState([]);
+  const [loadingUsers, setLoadingUsers] = useState(false);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
+  const fetchUsers = async () => {
+    setLoadingUsers(true);
+    try {
+      const {
+        data: { data, total },
+      } = await usersApi.fetchAllUsers({
+        page: currentPage,
+        limit: 10,
+      });
+      console.log(data);
+      setUsers([...data]);
+      setTotalPages(Math.floor(total / 10));
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setLoadingUsers(false);
+    }
+  };
+
+  const nextPage = () => {
+    if (currentPage < totalPages && !loadingUsers) {
+      setCurrentPage((currentPage) => currentPage + 1);
+      fetchUsers();
+    }
+  };
+
+  const prevPage = () => {
+    if (currentPage >= 0 && !loadingUsers) {
+      setCurrentPage((currentPage) => currentPage - 1);
+      fetchUsers();
+    }
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  return (
+    <div>
+      <h1>Users list</h1>
+      <Pagination
+        currentPage={currentPage}
+        totalPages={totalPages}
+        nextPage={nextPage}
+        prevPage={prevPage}
+      />
+      {loadingUsers ? <h1>Loading...</h1> : <TableView users={users} />}
+    </div>
+  );
+};
+
+export default UsersList;
---
+++ src/Users/UsersList/table_constants.js
@@ -0,0 +1,22 @@
+export const HEADERS = [
+  {
+    key: "id",
+    label: "Id",
+  },
+  {
+    key: "title",
+    label: "Title",
+  },
+  {
+    key: "firstName",
+    label: "First Name",
+  },
+  {
+    key: "lastName",
+    label: "Last Name",
+  },
+  {
+    key: "picture",
+    label: "Image Url",
+  },
+];
